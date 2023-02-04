3fa0bcd659efa81721cb938dcd91fe8b32a22ad0
bekadev <bekapng@gmail.com>
2023-02-04 23:07:31 +0600

    commit

--- src/01/01_01.test.ts
+++ src/01/01_01.test.ts
@@ -1,15 +1,10 @@
 import {twoFactor} from './01_01';
 
 test('twoFactor using for sum two value', () => {
-    //dates
-    //dates
-    //dates
     const date = {
         a: 1,
         b: 2
     }
-    //actions
     const result = twoFactor(date.a, date.b)
-    //expect
     expect(result).toBe(2)
 })
